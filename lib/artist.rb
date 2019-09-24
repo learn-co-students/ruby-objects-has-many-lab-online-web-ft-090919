490a796ca08560ee8f35fc2549136d23de649ecb
BryantIT <dragonseyeimages@gmail.com>
2019-09-24 12:57:09 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,32 +1,25 @@
 class Artist
   attr_accessor :name
 
-  @@song_count = 0
-
-  def initialize(name)
+    def initialize(name)
     @name = name
-    @songs = []
   end
 
   def songs
-    @songs
+    Song.all.select {|x| x.artist == self}
   end
 
   def add_song(song)
     song.artist = self
-    @songs << song
-    @@song_count += 1
   end
 
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     song.artist = self
-    @songs << song
-    @@song_count += 1
   end
 
   def self.song_count
-    @@song.count
+    Song.all.length
   end
 
 end
