e7fabe393e50ec95b3edfa3fc0f3de8aad7243ce
Shaqqour M <mshaqqourm@gmail.com>
2019-09-25 01:28:21 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,27 +1,24 @@
 require_relative "./post.rb"
 class Author
   attr_accessor :name
+  @@all = []
   
   def initialize(name)
     @name = name
+    @@all << self
   end
   def posts
     Post.all.select { |post| post.author = self }
   end
+  def add_post(post)
+    post.author = self
+  end
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.add_post(post)
+  end
+  def self.post_count
+    Post.all.count
+  end
 end
 
-
-  # def add_post(post)
-  #   post.author = self
-  # end
-  # def add_post_by_title(title)
-  #   post = Post.new(title)
-  #   self.add_song(song)
-  # end
-  # def self.song_count
-  #   count = 0
-  #   @@all.each do |artist|
-  #     count += artist.songs.count
-  #   end
-  #   count
-  # end
